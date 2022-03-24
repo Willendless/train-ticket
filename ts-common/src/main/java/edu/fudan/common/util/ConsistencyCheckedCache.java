7541adbfe60521f6d820e75cfd4b4666097905be
Willendless <317500141@qq.com>
2022-03-24 01:32:17 -0400

    feat(cache): update metrics output

--- ts-common/src/main/java/edu/fudan/common/util/ConsistencyCheckedCache.java
+++ ts-common/src/main/java/edu/fudan/common/util/ConsistencyCheckedCache.java
@@ -10,7 +10,11 @@ import java.util.function.BiFunction;
 public class ConsistencyCheckedCache<K, T, V> extends LinkedHashMap<K, V> {
 
     private String name;
+    // currently cacheSize is not used
     private int cacheSize;
+    private int queryCount;
+    private int hitCount;
+    private int coldMiss;
     // Key, extra argument, value
     private BiFunction<K, T, V> getter;
 
@@ -21,21 +25,34 @@ public class ConsistencyCheckedCache<K, T, V> extends LinkedHashMap<K, V> {
         this.name = name;
         this.cacheSize = cacheSize;
         this.getter = getter;
+        queryCount = 0;
+        hitCount = 0;
+        coldMiss = 0;
     }
 
     public V getOrInsert(K key, T extraArg) {
         V result = getter.apply(key, extraArg);
         V cached = get(key);
+
+        queryCount += 1;
+
         if (cached == null) {
+            coldMiss += 1;
+
             ConsistencyCheckedCache.LOGGER.info(
                     "[" + name + "] inserting entry for key "
                             + key
                             + ", value was "
                             + result);
             put(key, result);
+
+            printMetrics();
+
             return result;
         } else {
             if (cached.equals(result)) {
+                hitCount += 1;
+
                 ConsistencyCheckedCache.LOGGER.info(
                         "[" + name + "] had a good cached entry for key "
                                 + key
@@ -49,9 +66,20 @@ public class ConsistencyCheckedCache<K, T, V> extends LinkedHashMap<K, V> {
                                 + cached
                                 + ", actual value was "
                                 + result);
+                put(key, result);
             }
+
+            printMetrics();
+
             return cached;
         }
+    }
+
+    private void printMetrics() {
+        ConsistencyCheckedCache.LOGGER.info(
+            "[query count] " + queryCount + " "
+          + "[hit count] " + hitCount + " "
+          + "[cold miss] " + coldMiss);
     }
 
     protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
